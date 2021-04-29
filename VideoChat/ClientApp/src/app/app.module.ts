4646b2937b8c858426bf3752308069cdad297c75
Hari Krishna <Harik@touchcoresystems.com>
2021-04-29 17:55:06 +0530

    Removed dedundant templates

--- VideoChat/ClientApp/src/app/app.module.ts
+++ VideoChat/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CameraComponent } from './camera/camera.component';
 import { ParticipantsComponent } from './participants/participants.component';
 import { RoomsComponent } from './rooms/rooms.component';
@@ -20,8 +18,6 @@ import { DeviceSelectComponent } from './settings/device-select.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CameraComponent,
     ParticipantsComponent,
     RoomsComponent,
@@ -34,8 +30,6 @@ import { DeviceSelectComponent } from './settings/device-select.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
